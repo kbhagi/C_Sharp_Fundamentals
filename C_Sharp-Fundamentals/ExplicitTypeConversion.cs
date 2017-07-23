918e0d3ca07dd933b15a536c0366fdc2fbbde931
bhagi <bhargavak37@gmail.com>
2017-07-23 17:38:09 +0530

    Non_Compatible_types

---
+++ C_Sharp-Fundamentals/ExplicitTypeConversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class ExplicitTypeConversion
+    {
+        static void Main(String[] args)
+        {
+            float f = 1.0f;//compiler is aware of data loss, so we say explicitly that we want to cast to int
+            int i = (int)f;
+        }
+    }
+
+}
---
+++ C_Sharp-Fundamentals/Non_Compatible_types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Non_Compatible_types
+    {
+        static void Main(string[] args)
+        {
+            string s = "1";
+            int i = (int)s; //won't compile
+
+            string z = "1";
+            int x = Convert.ToInt32(z);//right way of conversion 
+            int j = int.Parse(z);
+        }
+    }
+}
