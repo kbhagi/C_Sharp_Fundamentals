16aaa628a2ac38ed75f8a3063ce3fd93975674e4
bhagi <bhargavak37@gmail.com>
2017-07-23 17:31:01 +0530

    ImplicitTypeConversion

---
+++ C_Sharp-Fundamentals/ImplicitTypeConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class ImplicitTypeConversion
+    {
+        static void Main(String[]args)
+        {
+            byte b = 1;//takes 1 byte of memory
+            int i = b; //implicit type conversion ,compiler knows byte can hold 8 bits, int can hold 32 bits
+
+            int x = 1; // chance for data loss
+            byte y = x; // won't compile 
+            //When the compiler knows theres is chance for data loss, it wont allow implicit inversino
+
+            int m = 1;
+            byte n = (byte)m; // Explicit type conversion
+        }
+
+    }
+}
