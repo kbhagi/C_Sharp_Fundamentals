62450ee855024d2e0a4315b0b8d061aaeee26eae
bhagi <bhargavak37@gmail.com>
2017-07-23 15:21:38 +0530

    identifier Notation

--- C_Sharp-Fundamentals/Program.cs
+++ C_Sharp-Fundamentals/Program.cs
@@ -11,6 +11,19 @@ namespace C_Sharp_Fundamentals //collection of classes
         static void Main(string[] args) //entry point to application 
         {
             Console.WriteLine("Hello World"); // CLR executes the code inside here
+
+            int number;
+            int Number = 1;
+            const float Pi = 3.14f;//this is a constant start with keyword const, identifier
+
+            identifier ex: oneRouter
+             Use meaningful names fn Camel Case: firstName
+            Pascal Case: FirstName
+            Hungarian Notation : strFirstName
+
+            for local variable: Camel Case  int number;
+            for constants: Pascal Case  const int MaxZoom
+
         }
     }
 }
