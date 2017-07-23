ff7dadb9e4ded999f964f8b0b7404dd84e5b008f
bhagi <bhargavak37@gmail.com>
2017-07-23 23:19:52 +0530

    Operators

---
+++ C_Sharp-Fundamentals/Operators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Operators
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a - b);
+            Console.WriteLine((float)a / (float)b);//explicit cast to float
+
+        }
+    }
+}
