ea0801d7a93d2e07a8fa4ea7026f3b382005fee5
bhagi <bhargavak37@gmail.com>
2017-07-23 14:58:59 +0530

    First program towards learning

--- C_Sharp-Fundamentals/Program.cs
+++ C_Sharp-Fundamentals/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C_Sharp_Fundamentals
+namespace C_Sharp_Fundamentals //collection of classes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //entry point to application 
         {
+            Console.WriteLine("Hello World"); // CLR executes the code inside here
         }
     }
 }
