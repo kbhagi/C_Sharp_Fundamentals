a0289a5c34e9f1c7e8cfb667958d1f634520c0c4
bhagi <bhargavak37@gmail.com>
2017-07-23 17:20:33 +0530

    new

---
+++ C_Sharp-Fundamentals/Overflowing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Overflowing
+    {
+        static void main(String[] args)
+        { 
+        byte number = 255;
+        number = number+1;
+
+            checked // prevent overflow & throw exception
+            {
+                byte number = 255;
+                number = number + 1;
+
+            }
+        }
+    }
+
---
+++ C_Sharp-Fundamentals/Scope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Scope
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;Pi = 1;
+        }
+    }
+}
