b20f7a1c24bb2b65cbee5ed11e8db195f31fcb6c
bhagi <bhargavak37@gmail.com>
2017-07-25 04:35:43 +0530

    Class, memory allocation with new keyword, static access , methods declared static , Multiple copy vs single copy.

---
+++ C_Sharp-Fundamentals/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using C_Sharp_Fundamentals.Math;
+
+namespace C_Sharp_Fundamentals
+{
+   
+    class Class
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+  
+
+
+}
