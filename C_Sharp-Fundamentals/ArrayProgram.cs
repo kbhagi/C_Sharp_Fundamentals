8d9d8982ebbf4abbc9d9226493e15a8891a01ca4
bhagi <bhargavak37@gmail.com>
2017-07-25 05:23:02 +0530

    ArrayProgram

---
+++ C_Sharp-Fundamentals/ArrayProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class ArrayProgram
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+        }
+    }
+}
