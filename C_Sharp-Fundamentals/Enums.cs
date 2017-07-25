73ad398801f9f7b27cd7d28ad1f76f56a422e533
bhagi <bhargavak37@gmail.com>
2017-07-26 02:39:02 +0530

    Enums

---
+++ C_Sharp-Fundamentals/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail=1,
+        RegisteredAirMail=2,
+        Express=3
+    }
+    class Enums
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+            Console.WriteLine(method.ToString());
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
