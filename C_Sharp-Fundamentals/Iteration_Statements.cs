863fa9a18190658437a71ca3cebdaa658a4c8db7
bhagi <bhargavak37@gmail.com>
2017-07-28 01:11:27 +0530

    Iteration_Statements

---
+++ C_Sharp-Fundamentals/Iteration_Statements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Iteration_Statements
+    {
+        static void Main(string[] args)
+        {                           //Anything that has a list like nature is IEnumerable ex: string is IEnumerable
+            for(var i=0;i<10;i++)
+            {
+
+            }
+            foreach (var number in numbers)
+            {
+
+            }
+            while (i <10)
+                {
+                    i++;
+                }
+            do
+            {
+                i++;
+            }while (if < 10)
+                Break: jump out of the loop
+                continue: jumps to the next iteration
+
+        }
+    }
+}
+ 
