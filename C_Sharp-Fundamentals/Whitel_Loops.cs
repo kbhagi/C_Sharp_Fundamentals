2dbab829449d508382d334a60a5994c2fe607d81
bhagi <bhargavak37@gmail.com>
2017-07-28 21:03:10 +0530

    While_Loops

---
+++ C_Sharp-Fundamentals/Whitel_Loops.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class While_Loops
+    {
+static void Main(string[] args)
+        {
+            var i = 0; while (i <= 10) { if (i % 2 == 0) Console.WriteLine(i); i++; }
+while(true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                { 
+                Console.WriteLine("@Echo: "+input);
+            }
+
+        }
+    }
+}
