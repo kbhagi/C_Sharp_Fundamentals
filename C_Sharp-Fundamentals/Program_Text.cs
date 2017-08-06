552cb3bd54d4d74815c6ccac4e8e69d40bf18011
bhagi <bhargavak37@gmail.com>
2017-08-06 10:05:23 +0530

    Summarize words in a sentence using string methods.

---
+++ C_Sharp-Fundamentals/Program_Text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_Sharp_Fundamentals
+{
+    class Program_Text
+    {
+static void Main(string[]args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
+            
+            }
+        
+
+        }
+    
+}
