ca15c4d2f3a116fe3284fa47e613df49f77b4600
bhagi <bhargavak37@gmail.com>
2017-08-07 19:57:03 +0530

    StringBuilder Make your code cleaner new ways in c#

---
+++ C_Sharp-Fundamentals/Program_StringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Program_StringBuilder
+    {
+        static void Main(string[]args)
+        {
+            //StringBuilder & String Manipulation methods
+            var builder = new StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+            Console.WriteLine("First Char: "+builder[0]);
+        }
+    }
+}
