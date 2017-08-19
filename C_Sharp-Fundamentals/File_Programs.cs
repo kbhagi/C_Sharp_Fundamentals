7c6638b8d078696cb022fc4405f5d0871b1b4106
bhargavak <bhargavak37@gmail.com>
2017-08-19 08:33:53 +0530

    few ups

---
+++ C_Sharp-Fundamentals/File_Programs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class File_Programs
+    {
+ static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg",true);
+            File.Delete(path);
+            if(File.Exists(path))
+            {
+
+            }
+            var content = File.ReadAllText(path);
+        }       
+    }
+}
