f0a4c3d4d510e16a0aa7eabaa8938fb9c797ea25
bhargavak <bhargavak37@gmail.com>
2017-08-20 18:06:08 +0530

    Path class in C# , Path.GetExtension(), Path.GetDirectoryName(),Path.GetFileName()

---
+++ C_Sharp-Fundamentals/PathProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class PathProgram
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\Projects\C_Sharp-Fundamentals\C_Sharp-Fundamentals.sln";
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+            Console.WriteLine("Extension: "+ Path.GetExtension(path));
+            Console.WriteLine("File Name: "+Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: "+Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: "+Path.GetDirectoryName(path));
+
+        }
+    }
+}
