fac2e5522bf5be7a1a0bc8b130fe8cf1428e241c
bhargavak <bhargavak37@gmail.com>
2017-08-20 17:12:27 +0530

    File provides static methods, when using the File class operating system does some security checks and that can impact the performance of your application if you have large number of operations. In that case your can create fileInfo object and use it. fileInfo provides instance methods

--- C_Sharp-Fundamentals/File_Programs.cs
+++ C_Sharp-Fundamentals/File_Programs.cs
@@ -11,6 +11,7 @@ namespace C_Sharp_Fundamentals
     {
  static void Main(string[] args)
         {
+            //File provides static methods, when using the File class operating system does some security checks and that can impact the performance of your application if you have large number of operations. In that case your can create fileInfo object and use it.
             var path = @"c:\somefile.jpg";
             File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg",true);
             File.Delete(path);
@@ -19,6 +20,15 @@ namespace C_Sharp_Fundamentals
 
             }
             var content = File.ReadAllText(path);
+            //fileInfo provides instance methods
+            var fileInfo = new FileInfo(path); 
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+           if (fileInfo.Exists)
+            {
+                //
+            }
+          
         }       
     }
 }
