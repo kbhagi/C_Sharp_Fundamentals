af05cdc524314dd7941b4aac7890485870ac8b76
bhargavak <bhargavak37@gmail.com>
2017-08-20 17:42:46 +0530

    Directory & DirectoryInfo in C#

---
+++ C_Sharp-Fundamentals/DirectoryInfoProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class DirectoryInfoProgram
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+           var files= Directory.GetFiles(@"D:\Projects\C_Sharp-Fundamentals", "*sln", SearchOption.AllDirectories);
+            //foreach (var file in files)
+               // Console.WriteLine(file);
+            var directories = Directory.GetDirectories(@"D:\Projects\C_Sharp-Fundamentals", "*.*", SearchOption.AllDirectories);
+     foreach(var directory in directories)
+                Console.WriteLine(directory);
+            Directory.Exists("...");
+            var directoryInfo = new DirectoryInfo("....");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+                       
+   }
+    }
+}
